f1779311d68472b535a99b40c4b252abec6e06a0
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2024-01-31 16:03:54 -0300

    feat: add a controller for (cliente)

---
+++ src/main/java/com/spring/validacao/controller/ClienteController.java
@@ -0,0 +1,21 @@
+package com.spring.validacao.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.validacao.entities.Cliente;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteController {
+
+	@PostMapping("/salvar")
+	public ResponseEntity<Cliente> save(@RequestBody @Valid Cliente cliente) {
+		return ResponseEntity.ok().body(cliente);
+	}
+}
