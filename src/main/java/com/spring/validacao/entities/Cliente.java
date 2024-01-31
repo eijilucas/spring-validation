fcaa48d4ae2681cd39b43125eba71ce58b6fbe41
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2024-01-31 15:57:41 -0300

    feat: new entity (cliente)

---
+++ src/main/java/com/spring/validacao/entities/Cliente.java
@@ -0,0 +1,92 @@
+package com.spring.validacao.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	private String name;
+	@Email
+	private String email;
+	@CPF
+	private String cpf;
+
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(Long id, @NotBlank String name, @Email String email, @CPF String cpf) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.cpf = cpf;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
+	}
+	
+}
